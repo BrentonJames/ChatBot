71f69e2fa84f9a63bd13508aa81cf9c6dccd23cb
BrentonJames <brentonjames888@gmail.com>
2021-04-23 10:26:27 +1000

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Alter_Ego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dialogue = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mic_toggle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.btn_Enter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 91);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Speak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(338, 120);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(102, 84);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // dialogue
+            // 
+            this.dialogue.AutoSize = true;
+            this.dialogue.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dialogue.ForeColor = System.Drawing.Color.SpringGreen;
+            this.dialogue.Location = new System.Drawing.Point(11, 372);
+            this.dialogue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.dialogue.Name = "dialogue";
+            this.dialogue.Size = new System.Drawing.Size(60, 21);
+            this.dialogue.TabIndex = 2;
+            this.dialogue.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 356);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.label1.Location = new System.Drawing.Point(428, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "/function_mode";
+            // 
+            // mic_toggle
+            // 
+            this.mic_toggle.AutoSize = true;
+            this.mic_toggle.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mic_toggle.ForeColor = System.Drawing.Color.SpringGreen;
+            this.mic_toggle.Location = new System.Drawing.Point(495, 419);
+            this.mic_toggle.Name = "mic_toggle";
+            this.mic_toggle.Size = new System.Drawing.Size(79, 21);
+            this.mic_toggle.TabIndex = 5;
+            this.mic_toggle.Text = "/mic_on";
+            this.mic_toggle.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SpringGreen;
+            this.label2.Location = new System.Drawing.Point(11, 419);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ">";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.SpringGreen;
+            this.richTextBox2.Location = new System.Drawing.Point(38, 417);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ShortcutsEnabled = false;
+            this.richTextBox2.Size = new System.Drawing.Size(348, 31);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox2_KeyDown);
+            // 
+            // btn_Enter
+            // 
+            this.btn_Enter.BackColor = System.Drawing.Color.Black;
+            this.btn_Enter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Enter.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Enter.ForeColor = System.Drawing.Color.SpringGreen;
+            this.btn_Enter.Location = new System.Drawing.Point(392, 416);
+            this.btn_Enter.Name = "btn_Enter";
+            this.btn_Enter.Size = new System.Drawing.Size(75, 31);
+            this.btn_Enter.TabIndex = 8;
+            this.btn_Enter.Text = "Enter";
+            this.btn_Enter.UseVisualStyleBackColor = false;
+            this.btn_Enter.Click += new System.EventHandler(this.btn_Enter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(586, 449);
+            this.Controls.Add(this.btn_Enter);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mic_toggle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dialogue);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Alter Ego";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label dialogue;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mic_toggle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button btn_Enter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace Alter_Ego
+{
+    public partial class Form1 : Form
+    {
+        bool micOn = true;
+        //bool search = false;
+        SpeechSynthesizer voiceRecog = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SpeechRecognitionEngine reco = new SpeechRecognitionEngine();
+            voiceRecog.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+            Choices list = new Choices();
+
+            dialogue.Text = (greetRandom());
+            voiceRecog.SpeakAsync(greetRandom());
+            //dialogue.Text = (greetRandom());
+            list.Add(new string[] { "hi", "hello", "open google", "open notepad", "close", "minimize", "fullscreen", "normal", "exit", "what is the date today", "what is the time", "order pizza" });
+
+            Grammar gm = new Grammar(new GrammarBuilder(list));
+
+
+
+            try
+            {
+                reco.RequestRecognizerUpdate();
+                reco.LoadGrammar(gm);
+                reco.SpeechRecognized += Reco_SpeechRecognized;
+                reco.SetInputToDefaultAudioDevice();
+                reco.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch { }
+        }
+
+        public string greetRandom()
+        {
+            string[] greetings = new string[3] { "Welcome back!", "Nice to see you again!", "How can I help you?" };
+            Random r = new Random();
+            return greetings[r.Next(3)];
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (micOn == true)
+            {
+                voiceRecog.Pause();
+                mic_toggle.Text = "/mic_off";
+                micOn = false;
+            }
+            else 
+            {
+                voiceRecog.Resume();
+                mic_toggle.Text = "/mic_on";
+                micOn = true;
+            }
+        }
+
+        private void Reco_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (micOn == true)
+            {
+                string a = e.Result.Text;
+                {
+                    switch (a)
+                    {
+
+                        // These are for functional commands
+
+                        case ("hi"):
+                            dialogue.Text = ("Hi!");
+                            voiceRecog.SpeakAsync("hi");
+                            //dialogue.Text = ("Hi!");
+                            break;
+
+                        case ("hello"):
+                            dialogue.Text = ("Hello, how are you?");
+                            voiceRecog.SpeakAsync("hello how are you");
+                            //dialogue.Text = ("Hello, how are you?");
+                            break;
+
+                        case ("open google"):
+                            dialogue.Text = ("Opening Google.");
+                            voiceRecog.SpeakAsync("opening google");
+                            //dialogue.Text = ("Opening Google.");
+                            Process.Start("https://www.google.com");
+                            break;
+
+                        case ("open notepad"):
+                            dialogue.Text = ("Opening Notepad.");
+                            voiceRecog.SpeakAsync("opening notepad");
+                            //dialogue.Text = ("Opening Notepad.");
+                            Process.Start("notepad.exe");
+                            break;
+
+                        case ("close"):
+                            //COME BACK AND FIX THIS ONE LATER
+                            //dialogue.Text = ("See you soon!");
+                            //voiceRecog.Speak("see you soon");
+                            //dialogue.Text = ("See you soon!");
+                            CloseSystem();
+                            break;
+
+                        case ("fullscreen"):
+                            dialogue.Text = ("That command has been disabled.");
+                            voiceRecog.SpeakAsync("that command has been disabled");
+                            //dialogue.Text = ("That command has been disabled.");
+                            //WindowState = FormWindowState.Maximized;
+                            break;
+
+                        case ("minimize"):
+                            WindowState = FormWindowState.Minimized;
+                            break;
+
+                        case ("normal"):
+                            //dialogue.Text = ("That command has been disabled.");
+                            //voiceRecog.Speak("that command has been disabled");
+                            //dialogue.Text = ("That command has been disabled.");
+                            WindowState = FormWindowState.Normal;
+                            break;
+
+                        case ("exit"):
+                            SendKeys.Send("%{F4}");
+                            break;
+
+                        case ("what is the date today"):
+                            dialogue.Text = ("Today is " + DateTime.Now.ToString());
+                            voiceRecog.SpeakAsync("Today is " + DateTime.Now.ToString());
+                            //dialogue.Text = ("Today is " + DateTime.Now.ToString());
+                            break;
+
+                        case ("what is the time"):
+                            dialogue.Text = ("Current time is " + DateTime.Now.ToShortTimeString());
+                            voiceRecog.SpeakAsync("Current time is " + DateTime.Now.ToShortTimeString());
+                            //dialogue.Text = ("Current time is " + DateTime.Now.ToShortTimeString());
+                            break;
+
+                        case ("order pizza"):
+                            dialogue.Text = ("Redirecting to Dominos.");
+                            voiceRecog.SpeakAsync("redirecting to dominoes");
+                            //dialogue.Text = ("Opening Google.");
+                            Process.Start("https://www.dominos.com.au/");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer voice = new SpeechSynthesizer();
+            string stringText;
+            stringText = richTextBox1.Text;
+            voice.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+            voice.Speak(stringText);
+        }
+
+        private void CloseSystem()
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Enter_Click(object sender, EventArgs e)
+        {
+            runCommand();
+        }
+
+        
+
+        private void runCommand()
+        {
+
+            voiceRecog.Resume();
+            string a = richTextBox2.Text;
+            SpeechSynthesizer inputVoice = new SpeechSynthesizer();
+            string stringText;
+            stringText = dialogue.Text;
+            inputVoice.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+            {
+                switch (a)
+                {
+
+                    // These are for functional commands
+
+                    case ("hi"):
+                        dialogue.Text = ("Hi!");
+                        voiceRecog.SpeakAsync("hi");
+                        break;
+
+                    case ("hello"):
+                        dialogue.Text = ("Hello, how are you?");
+                        voiceRecog.SpeakAsync("hello how are you");
+                        break;
+
+                    case ("open google"):
+                        dialogue.Text = ("Opening Google.");
+                        voiceRecog.SpeakAsync("opening google");
+                        Process.Start("https://www.google.com");
+                        break;
+
+                    case ("open notepad"):
+                        dialogue.Text = ("Opening Notepad.");
+                        voiceRecog.SpeakAsync("opening notepad");
+                        Process.Start("notepad.exe");
+                        break;
+
+                    case ("close"):
+                        CloseSystem();
+                        break;
+
+                    case ("fullscreen"):
+                        dialogue.Text = ("That command has been disabled.");
+                        voiceRecog.SpeakAsync("that command has been disabled");
+                        break;
+
+                    case ("minimize"):
+                        WindowState = FormWindowState.Minimized;
+                        break;
+
+                    case ("normal"):
+                        WindowState = FormWindowState.Normal;
+                        break;
+
+                    case ("exit"):
+                        SendKeys.Send("%{F4}");
+                        break;
+
+                    case ("what is the date today"):
+                        dialogue.Text = ("Today is " + DateTime.Now.ToString());
+                        voiceRecog.SpeakAsync("Today is " + DateTime.Now.ToString());
+                        break;
+
+                    case ("what is the time"):
+                        dialogue.Text = ("Current time is " + DateTime.Now.ToShortTimeString());
+                        voiceRecog.SpeakAsync("Current time is " + DateTime.Now.ToShortTimeString());
+                        break;
+
+                    case ("order pizza"):
+                        dialogue.Text = ("Redirecting to Dominos.");
+                        voiceRecog.SpeakAsync("redirecting to dominoes");
+                        Process.Start("https://www.dominos.com.au/");
+                        break;
+                }
+            }
+        }
+
+        private void richTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                runCommand();
+            }
+        }
+    }
+}
